c3fbecde7f06456ef70e7ec58dc13652fc476a7a
matr1x244 <zio-21@yandex.ru>
2023-04-17 19:49:46 +0300

    переписал checked через делегат by вместо = ;
    упростил код более читаемый

--- app/src/main/java/com/example/composeapril/Greeting.kt
+++ app/src/main/java/com/example/composeapril/Greeting.kt
@@ -1,28 +1,43 @@
 package com.example.composeapril
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
+@Preview(name = "Greeting", showBackground = true, showSystemUi = true)
 @Composable
-fun Greeting () {
-    val checked = remember { mutableStateOf(false) }
-    val checkedValue = checked.value
+fun Greeting() {
+    var checked by remember { mutableStateOf(false) }
 
     Column {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(checked = checkedValue, onCheckedChange = {value -> checked.value = value})
-            Text("Some important preference",  fontSize = 18.sp)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.align(CenterHorizontally)
+        ) {
+            Checkbox(checked = checked, onCheckedChange = { value -> checked = value })
+            Text("Some important preference", fontSize = 18.sp)
         }
-        if(checkedValue) {
-            Text("INVISIBLE TEXT VIEW :))")
+        if (checked) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Text("INVISIBLE TEXT VIEW :))")
+            }
+
         }
     }
 }
