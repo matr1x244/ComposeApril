340bb585722549aa09e4ea5fb7fbcda9041cff06
matr1x244 <zio-21@yandex.ru>
2023-04-17 19:53:09 +0300

    remember -> есть: rememberSaveable (У remember есть версия,которая способна сохранить значение даже при повороте экрана и завершении процесса. Это rememberSaveable.)

--- app/src/main/java/com/example/composeapril/Greeting.kt
+++ app/src/main/java/com/example/composeapril/Greeting.kt
@@ -6,22 +6,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 @Preview(name = "Greeting", showBackground = true, showSystemUi = true)
 @Composable
 fun Greeting() {
-    var checked by remember { mutableStateOf(false) }
+    var checked by rememberSaveable { mutableStateOf(false) }
 
+    /** remember -> есть:
+     *
+     * rememberSaveable (У remember есть версия,
+    которая способна сохранить значение даже при повороте экрана и завершении процесса. Это rememberSaveable.)
+     */
     Column {
         Row(
             verticalAlignment = Alignment.CenterVertically,
