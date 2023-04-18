2e7f03d21e0ccc7bc16d78b86ec01ff859c57a15
matr1x244 <zio-21@yandex.ru>
2023-04-18 20:23:50 +0300

    Добавил список итоговый вариант

--- app/src/main/java/com/example/composeapril/List.kt
+++ app/src/main/java/com/example/composeapril/List.kt
@@ -1,19 +1,35 @@
 package com.example.composeapril
 
+import android.widget.Toast
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun List(text: String) {
+    val context = LocalContext.current
+
     println("SomeItem $text")
     val list = remember { mutableListOf("cat 1", "dog 2", "unicorn 3") }
     Text(
         text = text, fontSize = 20.sp,
-        modifier = Modifier.padding(16.dp)
-    )
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .border(width = 1.dp, color = Color.Black)
+            .padding(2.dp)
+            .clickable {
+                Toast
+                    .makeText(context, "Text: clicker $text", Toast.LENGTH_SHORT)
+                    .show()
+            })
 }
--- app/src/main/java/com/example/composeapril/MainActivity.kt
+++ app/src/main/java/com/example/composeapril/MainActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.composeapril.ui.theme.ComposeAprilTheme
@@ -31,7 +35,9 @@ class MainActivity : ComponentActivity() {
 //                    Greeting()
 
                     val list = remember { mutableStateListOf("cat 1", "dog 2", "unicorn 3") }
-                    Column(Modifier.padding(0.dp, 30.dp)) {
+                    val scroll = rememberScrollState()
+                    Column(Modifier.padding(0.dp, 30.dp)
+                        .verticalScroll(scroll)) {
                         TextButton(onClick = {
                             list.add("Dragon ${list.size + 1}")
                         }) {
