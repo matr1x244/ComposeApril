ef437c0ef554a575d310cc0881707fdd1691cd01
matr1x244 <zio-21@yandex.ru>
2023-04-19 20:12:00 +0300

    Переписал ViewModel счётчик кликов на StateFlow

--- app/src/main/java/com/example/composeapril/viewmodel/ClickerClickerVM.kt
+++ app/src/main/java/com/example/composeapril/viewmodel/ClickerClickerVM.kt
@@ -3,6 +3,8 @@ package com.example.composeapril.viewmodel
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -10,10 +12,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 fun ClickerClickerVM() {
 
     val viewModel: ClickerViewModel = viewModel()
-    var counter = viewModel.counter
+    val counter by viewModel.counter.collectAsState()
 
     Text(
-        text = "Clicks: ${counter.value}",
+        text = "Clicks: $counter",
         modifier = Modifier.clickable(onClick = viewModel::onCounterClick)
     )
     println("$counter")
--- app/src/main/java/com/example/composeapril/viewmodel/ClickerViewModel.kt
+++ app/src/main/java/com/example/composeapril/viewmodel/ClickerViewModel.kt
@@ -3,11 +3,13 @@ package com.example.composeapril.viewmodel
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class ClickerViewModel: ViewModel() {
 
-    private val _counter = mutableStateOf(0)
-    val counter: State<Int> = _counter
+    private val _counter = MutableStateFlow(0)
+    val counter: StateFlow<Int> = _counter
 
     fun onCounterClick() {
         _counter.value++
