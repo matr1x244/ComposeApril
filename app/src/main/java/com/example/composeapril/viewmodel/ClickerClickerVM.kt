6189cba190c7014938d0cc4d939d9ab3cc8fb5e0
matr1x244 <zio-21@yandex.ru>
2023-04-19 20:24:37 +0300

    Переписал на более правильный вариант отвязал жесткую привзяка viewmodel от функционала Clickов

--- app/src/main/java/com/example/composeapril/viewmodel/ClickerClickerVM.kt
+++ app/src/main/java/com/example/composeapril/viewmodel/ClickerClickerVM.kt
@@ -13,21 +13,33 @@ fun ClickerClickerVM() {
 
     val viewModel: ClickerViewModel = viewModel()
     val counter by viewModel.counter.collectAsState()
+
     /**
      * Функция collectAsState подписывается на StateFlow и предоставляет State.
      * Каждый раз, когда значение в StateFlow меняется, обновляется и значение State.
      *
-        Получается такая схема подписки:
-        Composable -> State -> StateFlow
-    *
-        Composable читает значение из State, и тем самым подписывается на него,
-        чтобы перезапуститься при изменении значения. А State подписывается на StateFlow.
-        В итоге изменение значения в StateFlow будет приводить к перезапуску Composable функции.
+    Получается такая схема подписки:
+    Composable -> State -> StateFlow
+     *
+    Composable читает значение из State, и тем самым подписывается на него,
+    чтобы перезапуститься при изменении значения. А State подписывается на StateFlow.
+    В итоге изменение значения в StateFlow будет приводить к перезапуску Composable функции.
      */
+    
+    Clickers(count = counter, onCounterClick = viewModel::onCounterClick)
 
-    Text(
-        text = "Clicks: $counter",
-        modifier = Modifier.clickable(onClick = viewModel::onCounterClick)
-    )
-    println("$counter")
+    /**
+     * ClickerClickerVM берет все необходимые данные из модели и передает в Clickers.
+     * Теперь Clickers не привязан к ClickerViewModel и может быть использован на любых других экранах.
+     */
 }
+
+@Composable
+fun Clickers(count: Int, onCounterClick: () -> Unit) {
+    Text(
+        text = "Clicks: $count",
+        modifier = Modifier.clickable(onClick = onCounterClick)
+    )
+    println("$count")
+}
+
