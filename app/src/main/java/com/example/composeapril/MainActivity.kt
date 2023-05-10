fa50fbade8896c48215945cb1eb8f646b41e38dd
matr1x244 <zio-21@yandex.ru>
2023-05-10 20:44:13 +0300

    Default Navigation NavigationScreen @fun composable

--- app/src/main/java/com/example/composeapril/MainActivity.kt
+++ app/src/main/java/com/example/composeapril/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,21 +16,21 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.composeapril.navigation.NavigationHomeScreen
 import com.example.composeapril.navigation.NavigationSaleScreen
 import com.example.composeapril.navigation.NavigationWalletScreen
 import com.example.composeapril.ui.theme.ComposeAprilTheme
 
 class MainActivity : ComponentActivity() {
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -42,53 +41,59 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     /**
-                     * Navigation custom
+                     * Navigation default compose
                      */
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        var menu by remember { mutableStateOf("home") }
-                        Box(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(16.dp)
-                        ) {
-                            when (menu) {
-                                "home" -> {
-                                    NavigationHomeScreen()
-                                }
-
-                                "sale" -> {
-                                    NavigationSaleScreen()
-                                }
-
-                                "wallet" -> {
-                                    NavigationWalletScreen()
-                                }
-                            }
-                        }
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .size(60.dp)
-                                .padding(5.dp, 0.dp)
-                                .background(
-                                    Color.Red,
-                                    shape = RoundedCornerShape(
-                                        topStart = 15.dp,
-                                        topEnd = 15.dp,
-                                        bottomEnd = 0.dp,
-                                        bottomStart = 0.dp
-                                    )
-                                ),
-                            horizontalArrangement = Arrangement.SpaceEvenly,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Text(text = "home", modifier = Modifier.clickable { menu = "home" }, color = Color.White)
-                            Text(text = "sale", modifier = Modifier.clickable { menu = "sale" }, color = Color.White)
-                            Text(text = "wallet", modifier = Modifier.clickable { menu = "wallet" }, color = Color.White)
-                        }
-                    }
-
+                    NavigationScreen()
                 }
+            }
+        }
+    }
+
+    @Composable
+    private fun NavigationScreen() {
+        Column(modifier = Modifier.fillMaxSize()) {
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "home",
+                modifier = Modifier.weight(1f)
+            ) {
+                composable("home") { NavigationHomeScreen() }
+                composable("sale") { NavigationSaleScreen() }
+                composable("wallet") { NavigationWalletScreen() }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(60.dp)
+                    .padding(5.dp, 0.dp)
+                    .background(
+                        Color.Red,
+                        shape = RoundedCornerShape(
+                            topStart = 15.dp,
+                            topEnd = 15.dp,
+                            bottomEnd = 0.dp,
+                            bottomStart = 0.dp
+                        )
+                    ),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "home", modifier = Modifier
+                        .clickable { navController.navigate("home") },
+                    color = Color.White
+                )
+                Text(
+                    text = "sale",
+                    modifier = Modifier.clickable { navController.navigate("sale") },
+                    color = Color.White
+                )
+                Text(
+                    text = "wallet",
+                    modifier = Modifier.clickable { navController.navigate("wallet") },
+                    color = Color.White
+                )
             }
         }
     }
