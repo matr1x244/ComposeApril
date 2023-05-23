ba7983427f9248573a73826b498d080eaadc72e2
matr1x244 <zio-21@yandex.ru>
2023-05-23 20:51:09 +0300

    rememberUpdatedState на примере со slider с использованием
     val positionState = remember { mutableStateOf(position) }
    
     positionState.value = position

--- app/src/main/java/com/example/composeapril/MainActivity.kt
+++ app/src/main/java/com/example/composeapril/MainActivity.kt
@@ -1,36 +1,23 @@
 package com.example.composeapril
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import com.example.composeapril.rememberObserver.RememberObserverTestingLifeCycle
 import com.example.composeapril.ui.theme.ComposeAprilTheme
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.util.Date
 
 class MainActivity : ComponentActivity() {
 
@@ -42,83 +29,48 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    ComposableLifecycle()
+                    sliderPosition()
                 }
             }
         }
     }
 
+
+    /**
+     * Получилось следующее:
+    - remember хранит для нас State
+    - при каждом движении слайдера, идет вызов функции TrackPosition и мы пишем актуальное значение позиции слайдера в State
+    - корутина в LaunchedEffect раз в секунду читает актуальное значение позиции слайдера из State и выводит данные
+     */
+
     @Composable
-    private fun ComposableLifecycle() {
+    private fun sliderPosition() {
         Column {
-            var checked by remember { mutableStateOf(false) }
-            Checkbox(checked = checked, onCheckedChange = { checked = it })
-            val homeTime = remember { currentTime() }
-            val rememberObserverTestingLifeCycle = remember { RememberObserverTestingLifeCycle() }
-            if (checked) {
-                val timeScreenStopReally = remember { currentTime() }
-                val rememberObserverTestingLifeCycle =
-                    remember { RememberObserverTestingLifeCycle() }
-                Text("val homeTime Start = $homeTime \n time Really Screen = $timeScreenStopReally")
-                ClickCounter()
+            var sliderPosition by remember { mutableStateOf(1f) }
 
-//                var count by remember { mutableStateOf(0) }
+            Slider(
+                value = sliderPosition,
+                valueRange = 1f..10f,
+                onValueChange = { sliderPosition = it })
 
-                /**
-                 * produceState создает и возвращает нам State, а в его блоке кода (корутине) мы используем поле value,
-                 * чтобы менять значение этого State. Раз в секунду мы увеличиваем это значение на 1.
-                 *
-                 * В качестве полезного применения produceState обычно приводят пример с загрузкой данных.
-                 * Мы стартуем корутину, постим в value статус Loading и начинаем в этой корутине что-то загружать.
-                 * После завершения загрузки постим в value результат Result<...> или ошибку Error.
-                 */
-                val count by produceState(initialValue = 0) {
-                    while (true) {
-                        delay(1000)
-                        value++
-                    }
-                }
-                Text(text = "count = $count")
-
-//                LaunchedEffect(key1 = Unit) {
-//                    while (true) {
-//                        delay(1000)
-//                        count++
-//                    }
-//                }
-            }
+            TrackPosition(position = sliderPosition)
+            Text(text = sliderPosition.toString())
         }
-
     }
 
-    @Composable
-    fun BroadcastReceiver(intentFilter: IntentFilter, onReceive: (Intent) -> Unit) {
-        val context = LocalContext.current
-        DisposableEffect(context) {
-            val broadcastReceiver = object : BroadcastReceiver() {
-                override fun onReceive(context: Context, intent: Intent) {
-                    onReceive(intent)
-                }
-            }
-            context.registerReceiver(broadcastReceiver, intentFilter)
-            onDispose {
-                context.unregisterReceiver(broadcastReceiver)
+    private @Composable
+    fun TrackPosition(position: Float) {
+        val positionState = remember { mutableStateOf(position) }
+        positionState.value = position
+
+        LaunchedEffect(key1 = Unit) {
+            while(true) {
+                delay(1000)
+                println("@@@@ track position ${positionState.value}")
             }
         }
     }
 
-    @Composable
-    fun ClickCounter() {
-        var count by remember { mutableStateOf(0) }
-        Text(
-            text = "Count $count",
-            modifier = Modifier.clickable { count++ }
-        )
-    }
-
-    private fun currentTime(): String {
-        return SimpleDateFormat("HH:mm:ss").format(Date())
-    }
 
 }
 
